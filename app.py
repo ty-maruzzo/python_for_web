7a108991c11758ce4fcdef28b3cb4406c99b4a13
Tyrone Maruzzo <macbook@tyrones-macbook-pro.local>
2026-03-26 19:27:51 +0800

    Fix templates, add result route, allow HEAD

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect,url_for 
 import os
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/')
 def home():
     techs =['HTML','CSS','Flash','Python']
@@ -12,16 +13,21 @@ def about():
     name = '30 Days of Python Programming'
 
     return render_template('about.html',name = name, title = 'About Us')
-@app.route('/post')
+@app.route('/post', methods=['GET','POST'])
 def post():
     name = 'Text Analyzer'
-    return render_template('post.html',name=name,title = name)
+    if request.method == 'GET':
+        return render_template('post.html', name=name, title=name)
+    if request.method == 'POST':
+        content = request.form['content']
+        print(content)
+        return redirect(url_for('result'))
 
   
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT,",5000))
+    port = int(os.environ.get("PORT",5000))
     app.run(debug = True, host = '0.0.0.0', port = port
             )
     
